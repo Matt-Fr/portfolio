4d3b45f2de7e42695c525291dd9cf3695e9d70a2
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-07-04 15:53:55 +0200

    add a btn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section>
+      <h1>Hey my name is Matthieu FR, check out my projects</h1>
+      <Link to="/projects">View Projects</Link>
+    </section>
+  );
 };
 
 export default Home;
