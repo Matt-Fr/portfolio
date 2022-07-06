30adae50d083fd2ab5f15a2ffdd6e5244e0ec995
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-07-06 15:29:09 +0200

    added pic and started articles

---
+++ src/data.js
@@ -0,0 +1,21 @@
+import kasaPic from "./assets/kasa.jpg";
+
+const dataEn = [
+  {
+    id: 1,
+    title: "Kasa",
+    picture: kasaPic,
+    description:
+      "I'm baby brunch migas YOLO twee pickled gluten-free. Hell of vape art party, tote bag poutine sartorial blue bottle hella la croix DIY microdosing ennui ramps. Vexillologist portland mlkshk keytar tousled. Flexitarian four dollar toast paleo yuccie tacos dreamcatcher thundercats microdosing hexagon wolf. Prism banh mi poutine, bicycle rights tonx air plant authentic vice farm-to-table deep v truffaut DSA mlkshk letterpress twee. Edison bulb cronut etsy before they sold out, roof party venmo twee palo santo.",
+    tools: "[React, HTML & CSS, Responsive]",
+  },
+  {
+    id: 2,
+    title: "FilmFinder",
+    picture: kasaPic,
+    description:
+      "I'm baby vexillologist stumptown swag, put a bird on it tote bag DIY meggings. Post-ironic lomo ethical etsy keffiyeh pitchfork wayfarers austin cold-pressed tofu blue bottle 8-bit helvetica butcher. Helvetica la croix austin air plant. Subway tile bespoke praxis dreamcatcher chia forage lyft humblebrag lumbersexual synth brunch la croix.",
+  },
+];
+
+export default dataEn;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useGlobalContext } from "../context";
+import dataEn from "../data";
+import kasa from "../assets/kasa.jpg";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="sectionProjects">
+      {dataEn.map((project) => {
+        const { id, title, description, picture } = project;
+        console.log(project);
+        return (
+          <article
+            onClick={(e) => {
+              console.log(e.currentTarget);
+            }}
+            key={id}
+            className="sectionProjects-article"
+          >
+            <h1>{title}</h1>
+            <img
+              src={kasa}
+              alt={title}
+              className="sectionProjects-article-img"
+            />
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Projects;
