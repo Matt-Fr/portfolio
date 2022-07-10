e8ec167ff3d505340be5721b1387783e3baab7fb
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-07-10 15:56:16 +0200

    added links demo and github

--- src/data.js
+++ src/data.js
@@ -20,6 +20,8 @@ const data = [
     descriptionFr:
       "I'm baby brunch migas YOLO twee pickled gluten-free. Hell of vape art party, tote bag poutine sartorial blue bottle hella la croix DIY microdosing ennui ramps. Vexillologist portland mlkshk keytar tousled. Flexitarian four dollar toast paleo yuccie tacos dreamcatcher thundercats microdosing hexagon wolf. Prism banh mi poutine, bicycle rights tonx air plant authentic vice farm-to-table deep v truffaut DSA mlkshk letterpress twee. Edison bulb cronut etsy before they sold out, roof party venmo twee palo santo.",
     tools: "[React, HTML & CSS, Responsive]",
+    demo: "https://mattfr-kasa.netlify.app/",
+    linkGithub: "https://github.com/Matt-Fr/Franck_Matthieu_11_26042022",
   },
   {
     id: 2,
@@ -31,6 +33,8 @@ const data = [
     descriptionFr:
       "Meggings bitters poutine reprehenderit chillwave bicycle rights. Leggings raw denim scenester, dreamcatcher vice ut cred freegan sed fixie aute wolf. Keffiyeh af hammock keytar edison bulb tilde, williamsburg lomo. Pork belly lo-fi roof party pop-up, aesthetic lomo tonx neutra PBR&B semiotics commodo laborum fugiat deep v four dollar toast. Shaman poutine ennui tbh, jianbing minim single-origin coffee williamsburg coloring book. Trust fund tumeric adaptogen stumptown you probably haven't heard of them 90's magna direct trade pitchfork skateboard pariatur letterpress fam gentrify.",
     tools: "[React, HTML & CSS, Responsive]",
+    demo: "https://mattfr-film-finder.netlify.app/",
+    linkGithub: "https://github.com/Matt-Fr/film_finder",
   },
   {
     id: 3,
@@ -42,6 +46,8 @@ const data = [
     descriptionFr:
       "Meggings bitters poutine reprehenderit chillwave bicycle rights. Leggings raw denim scenester, dreamcatcher vice ut cred freegan sed fixie aute wolf. Keffiyeh af hammock keytar edison bulb tilde, williamsburg lomo. Pork belly lo-fi roof party pop-up, aesthetic lomo tonx neutra PBR&B semiotics commodo laborum fugiat deep v four dollar toast. Shaman poutine ennui tbh, jianbing minim single-origin coffee williamsburg coloring book. Trust fund tumeric adaptogen stumptown you probably haven't heard of them 90's magna direct trade pitchfork skateboard pariatur letterpress fam gentrify.",
     tools: "[Vanilla JS, HTML & CSS, Responsive]",
+    demo: "https://matt-fr.github.io/FranckMatthieu_6_28102021/index.html",
+    linkGithub: "https://github.com/Matt-Fr/FranckMatthieu_6_28102021",
   },
   {
     id: 4,
@@ -53,6 +59,8 @@ const data = [
     descriptionFr:
       "Meggings bitters poutine reprehenderit chillwave bicycle rights. Leggings raw denim scenester, dreamcatcher vice ut cred freegan sed fixie aute wolf. Keffiyeh af hammock keytar edison bulb tilde, williamsburg lomo. Pork belly lo-fi roof party pop-up, aesthetic lomo tonx neutra PBR&B semiotics commodo laborum fugiat deep v four dollar toast. Shaman poutine ennui tbh, jianbing minim single-origin coffee williamsburg coloring book. Trust fund tumeric adaptogen stumptown you probably haven't heard of them 90's magna direct trade pitchfork skateboard pariatur letterpress fam gentrify.",
     tools: "[Vanilla JS, HTML & CSS, Responsive]",
+    demo: "https://mattfr-les-petits-plats.netlify.app/",
+    linkGithub: "https://github.com/Matt-Fr/FranckMatthieu_7_03012022",
   },
   {
     id: 5,
@@ -64,6 +72,8 @@ const data = [
     descriptionFr:
       "Meggings bitters poutine reprehenderit chillwave bicycle rights. Leggings raw denim scenester, dreamcatcher vice ut cred freegan sed fixie aute wolf. Keffiyeh af hammock keytar edison bulb tilde, williamsburg lomo. Pork belly lo-fi roof party pop-up, aesthetic lomo tonx neutra PBR&B semiotics commodo laborum fugiat deep v four dollar toast. Shaman poutine ennui tbh, jianbing minim single-origin coffee williamsburg coloring book. Trust fund tumeric adaptogen stumptown you probably haven't heard of them 90's magna direct trade pitchfork skateboard pariatur letterpress fam gentrify.",
     tools: "[Vanilla JS, HTML & CSS, Responsive]",
+    demo: "https://mattfr-ohmyfood.netlify.app/",
+    linkGithub: "https://github.com/Matt-Fr/Franckmatthieu_3_24082021",
   },
 ];
 
