623f4f1516166d455c94d42c0a0dd88f6d082505
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-07-13 19:03:18 +0200

    adjusted css navbar

--- src/components/LanguageBtn.js
+++ src/components/LanguageBtn.js
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import { useGlobalContext } from "../context";
 
 const LanguageBtn = () => {
   const { language, setLanguage, writeBilingualContent } = useGlobalContext();
+  const [isEnActive, setIsEnActive] = useState(true);
 
   return (
     <div className="langBtns">
       <button
+        className={
+          isEnActive ? "langBtns-btn langBtns-btn-active" : "langBtns-btn"
+        }
         onClick={() => {
           setLanguage("en");
+          setIsEnActive(true);
         }}
       >
         En
       </button>
       <button
+        className={
+          isEnActive ? "langBtns-btn" : "langBtns-btn langBtns-btn-active"
+        }
         onClick={() => {
           setLanguage("fr");
+          setIsEnActive(false);
         }}
       >
         Fr
