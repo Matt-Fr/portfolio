2f2aa4a05f10537be308a24147809ec1301f71a9
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-07-21 18:28:09 +0200

    try form on Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,6 +17,35 @@ const Home = () => {
       <Link to="/projects">
         {writeBilingualContent("View Projects", "Voir mes projets")}
       </Link>
+      <section>
+        <form
+          method="post"
+          name="contact v1"
+          data-netlify="true"
+          onSubmit="submit"
+        >
+          <input type="hidden" name="form-name" value="contact v1" />
+          <div>
+            <label htmlFor="first-name">
+              First name
+              <input type="text" name="first-name" />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="email">
+              Email
+              <input type="email" name="email" />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="">
+              any Comments?
+              <textarea name="comments"></textarea>
+            </label>
+          </div>
+          <button type="submit">Submit the result</button>
+        </form>
+      </section>
     </section>
   );
 };
