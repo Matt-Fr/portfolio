86e68ae4843f8deaed65d2518b0afbda2fe4fa82
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-07-22 17:39:20 +0200

    added more info contactform

--- src/components/FormContact.js
+++ src/components/FormContact.js
@@ -1,16 +1,51 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useGlobalContext } from "../context";
 
 const FormContact = () => {
+  const { language, setLanguage, writeBilingualContent } = useGlobalContext();
+  //   const [name, setName] = useState("");
+
+  //   const handleChange = (input) => {
+  //     setName(input.target.value);
+  //   };
+  //   useEffect(() => {
+  //     console.log(name);
+  //   }, [name]);
+
   return (
     <div className="contact">
       <form name="contact" method="post" data-netlify="true" onSubmit="submit">
         <input type="hidden" name="form-name" value="contact" />
-        <label for="name">Your Name: </label>
-        <input type="text" name="name" />
-        <label for="email">Your Email: </label>
-        <input type="email" name="email" />
-        <label for="message">Message: </label>
-        <textarea name="message"></textarea>
+        <label for="firstName"></label>
+        <input
+          type="text"
+          name="firstname"
+          // onChange={handleChange}
+          placeholder={writeBilingualContent("Firstname", "Prénom")}
+        />
+        <label for="lastname"></label>
+        <input
+          type="text"
+          name="lastname"
+          // onChange={handleChange}
+          placeholder={writeBilingualContent("Lastname", "Nom")}
+        />
+        <label for="company"></label>
+        <input
+          type="text"
+          name="company"
+          // onChange={handleChange}
+          placeholder={writeBilingualContent(
+            "Company (optional)",
+            "Entreprise (optionnel)"
+          )}
+        />
+        <label for="email"></label>
+        <input type="email" name="email" placeholder="Email" />
+        <label for="message"></label>
+        <textarea name="message" placeholder="Message"></textarea>
 
         <button type="submit">Send</button>
       </form>
