03388e873db6b48d8aeff441942a0772c309b962
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-08-03 16:34:10 +0200

    reorganized navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,32 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useGlobalContext } from "../context";
+import LanguageBtn from "../components/LanguageBtn";
+import ToggleTheme from "../components/ToggleTheme";
 
 const Navbar = () => {
   const { writeBilingualContent } = useGlobalContext();
   return (
-    <nav className="navbar">
-      <NavLink className="navbar-element" to="/">
-        {writeBilingualContent("Home", "Accueil")}
-      </NavLink>
-      <NavLink className="navbar-element" to="projects">
-        {writeBilingualContent("Projects", "Projets")}
-      </NavLink>
-      <NavLink className="navbar-element" to="contact">
-        {writeBilingualContent("Contact", "Contact")}
-      </NavLink>
-    </nav>
+    <>
+      <Link to="/" className="logo">
+        Matthieu Franck
+      </Link>
+      <nav className="navbar">
+        <NavLink className="navbar-element" to="/">
+          {writeBilingualContent("Home", "Accueil")}
+        </NavLink>
+        <NavLink className="navbar-element" to="projects">
+          {writeBilingualContent("Projects", "Projets")}
+        </NavLink>
+        <NavLink className="navbar-element" to="contact">
+          {writeBilingualContent("Contact", "Contact")}
+        </NavLink>
+      </nav>
+      <div className="switchBtns">
+        <LanguageBtn />
+        <ToggleTheme />
+      </div>
+    </>
   );
 };
 
--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Background from "../components/Background";
-import LanguageBtn from "../components/LanguageBtn";
 import Navbar from "../components/Navbar";
-import ToggleTheme from "../components/ToggleTheme";
 import "./SharedLayout.scss";
 
 const SharedLayout = () => {
@@ -11,14 +9,7 @@ const SharedLayout = () => {
     <>
       <Background />
       <header className="mainHeader">
-        <Link to="/" className="logo">
-          Matthieu Franck
-        </Link>
         <Navbar />
-        <div className="switchBtns">
-          <LanguageBtn />
-          <ToggleTheme />
-        </div>
       </header>
       <main>
         <Outlet />
