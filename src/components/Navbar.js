de83f0ac0e95737aa44416f2360fa9e81f5f4aa0
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-08-03 19:33:10 +0200

    created the hamburger menu for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useGlobalContext } from "../context";
 import LanguageBtn from "../components/LanguageBtn";
 import ToggleTheme from "../components/ToggleTheme";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks);
+  };
   const { writeBilingualContent } = useGlobalContext();
   return (
     <header className="mainHeader">
       <Link to="/" className="logo">
         Matthieu Franck
       </Link>
-      <nav className="navbar">
-        <NavLink className="navbar-element" to="/">
+      <nav className={showLinks ? "navbar show-nav" : "navbar"}>
+        <NavLink className="navbar-element" to="/" onClick={handleShowLinks}>
           {writeBilingualContent("Home", "Accueil")}
         </NavLink>
-        <NavLink className="navbar-element" to="projects">
+        <NavLink
+          className="navbar-element"
+          to="projects"
+          onClick={handleShowLinks}
+        >
           {writeBilingualContent("Projects", "Projets")}
         </NavLink>
-        <NavLink className="navbar-element" to="contact">
+        <NavLink
+          onClick={handleShowLinks}
+          className="navbar-element"
+          to="contact"
+        >
           {writeBilingualContent("Contact", "Contact")}
         </NavLink>
+        <button className="hamburger" onClick={handleShowLinks}>
+          <span className="hamburger-bar"></span>
+        </button>
       </nav>
       <div className="switchBtns">
         <LanguageBtn />
