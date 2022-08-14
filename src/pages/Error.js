4ad36df29be22f6403ad9d4654edf1a1a7db5a27
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-08-14 17:36:25 +0200

    add error page & description

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./Error.scss";
+import { useGlobalContext } from "../context";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  const { writeBilingualContent } = useGlobalContext();
+  return (
+    <section className="errorSection">
+      <h1 className="errorSection-title">
+        {writeBilingualContent("Error 404", "Erreur 404")}
+      </h1>
+      <Link className="errorSection-link" to="/">
+        {writeBilingualContent("Go back home !!", "Retournes à l'accueil !!")}
+      </Link>
+    </section>
+  );
 };
 
 export default Error;
