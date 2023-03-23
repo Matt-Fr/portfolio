40afdad0baed670a776ec46a558aa33b872dc0be
Matt-Fr <matthieu_franck@orange.fr>
2023-03-23 15:08:01 +0100

    added an eng version of my description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -44,14 +44,10 @@ const Home = () => {
           </a>
         </div>
         <p className="homeInfoContainer-description">
-          Après avoir travaillé dans le secteur du tourisme, j'ai décidé de me
-          reconvertir dans le développement web. J'ai suivi une formation pour
-          devenir développeur Front-End, et j'ai découvert ma passion pour
-          React. Toujours en quête de nouvelles connaissances, j'apprends
-          actuellement les technologies de demain telles que Next.js et
-          TypeScript. Je suis particulièrement intéressé par la recherche d'une
-          équipe composée de personnes aux talents variés, avec qui je pourrais
-          continuer à apprendre et à développer mes compétences.
+          {writeBilingualContent(
+            "After working in the tourism industry, I decided to switch to web development. I obtained a diploma to become a Front-End developer, and I discovered my passion for React. Always in search of new knowledge, I am currently learning upcoming technologies such as Next.js and TypeScript. I am particularly interested in finding a team with diverse talents, with whom I could continue to learn and develop my skills.",
+            "Après avoir travaillé dans le secteur du tourisme, j'ai décidé de me reconvertir dans le développement web. J'ai suivi une formation pour devenir développeur Front-End, et j'ai découvert ma passion pour React. Toujours en quête de nouvelles connaissances, j'apprends actuellement les technologies de demain telles que Next.js et TypeScript. Je suis particulièrement intéressé par la recherche d'une équipe composée de personnes aux talents variés, avec qui je pourrais continuer à apprendre et à développer mes compétences."
+          )}
         </p>
       </div>
       <div className="techContainer">
